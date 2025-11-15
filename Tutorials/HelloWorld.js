f2b9c64b7441eeae4da3558deaae4922d95e964b
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-15 12:30:38 +0530

    Learning modules

--- Tutorials/HelloWorld.js
+++ Tutorials/HelloWorld.js
@@ -1,9 +1,28 @@
+const math = require("./math")
+
+
+
+
+
+
+built in packages 
+// const cr = require("fs")
+// const cr = require("cypto")
+
+
+
 console.log("hey there Thats a print command  ");
 
 
 
 //           console.log(alert("heyy"));
 // thsi is not working as  using node js  we can have core fucntionality  of JS but not all the browser functioning 
+
+ console.log("learning modules ");
+
+console.log(math.add(2, 5 ));
+console.log(math.sub(2, 5 ));
+console.log(math.add(2, 5 ));
 
 
 
