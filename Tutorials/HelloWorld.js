0c9559de5586132d73ead47677a799e4a54b070d
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-15 12:10:12 +0530

    Running Npm comands / Scripts usage

---
+++ Tutorials/HelloWorld.js
@@ -0,0 +1,14 @@
+console.log("hey there Thats a print command  ");
+
+
+
+//           console.log(alert("heyy"));
+// thsi is not working as  using node js  we can have core fucntionality  of JS but not all the browser functioning 
+
+
+
+
+
+
+
+
