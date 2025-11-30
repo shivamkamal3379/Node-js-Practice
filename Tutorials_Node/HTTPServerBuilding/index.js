74fd1f6c8fd69403dea7803a9eb96013cf8e8fff
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-30 20:20:01 +0530

    Set Up all URl Things  with url Library

--- Tutorials_Node/HTTPServerBuilding/index.js
+++ Tutorials_Node/HTTPServerBuilding/index.js
@@ -1,23 +1,34 @@
 const http = require ("http");
 
-
+const url = require("url");
 
 const fs = require("fs");
-
+ 
 
 
 const myServer = http.createServer((req, res)=> {
+
     const log = `${Date.now()}:${req.url}" New Request Received\n`
-fs.appendFile('log.txt', log , (err, data)=>{
-    switch(req.url){
+    const myUrl = url.parse(req.url , true );
+    console.log(myUrl)
+    fs.appendFile('log.txt', log , (err, data)=>{
+
+    switch(myUrl.pathname){
         case '/' :res.end("HomePage");
         break;
-        case '/about' :res.end("i am shivam Kamal ");
+        case '/about' :
+             const userName = myUrl.query.myname
+            res.end(`hi i am ${userName}   `);
         break;
+
+        case '/search':
+            const searching = myUrl.query.search_query;
+            res.end(`${searching} is searched`);
+            break;
         default:
             res.end("404 Not Found")
     }
-    res.end("Hello from Server Dubara seee ");
+    // res.end("Hello from Server Dubara seee ");
     
 })
 });
