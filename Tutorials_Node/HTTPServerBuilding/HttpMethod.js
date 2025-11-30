e99356ecaa4b25356cd7a37963148311c4046de6
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-30 21:17:09 +0530

    Methods in HTTP

---
+++ Tutorials_Node/HTTPServerBuilding/HttpMethod.js
@@ -0,0 +1,46 @@
+const http = require ("http");
+
+const url = require("url");
+
+const fs = require("fs");
+ 
+
+
+const myServer = http.createServer((req, res)=> {
+
+    const log = `${Date.now()}:${req.url}" New Request Received\n`
+    const myUrl = url.parse(req.url , true );
+    console.log(myUrl)
+    fs.appendFile('log.txt', log , (err, data)=>{
+
+    switch(myUrl.pathname){
+        case '/' :res.end("Home Page");
+        break;
+        case '/about' :
+             const userName = myUrl.query.myname
+            res.end(`hi i am ${userName}   `);
+        break;
+
+        case '/search':
+            const searching = myUrl.query.search_query;
+            res.end(`${searching} is searched`);
+            break;
+        case '/Signup':
+            if(req.method ==='GET') res.end("This is a signUp Form ");
+            else if(req.method ==='POST'){
+                // DB Query
+                res.end("Success");
+            }
+            break ;
+        default:
+            res.end("404 Not Found")
+    }
+    // res.end("Hello from Server Dubara seee ");
+    
+})
+});
+
+myServer.listen(5555 , ()=>{
+    console.log("Server Started");
+});
+
