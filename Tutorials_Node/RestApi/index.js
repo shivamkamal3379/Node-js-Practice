dcda8a4ade0af36736e2787c94d45a22d3ecbbbc
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-04 11:03:15 +0530

    GET POST REST API DONE

--- Tutorials_Node/RestApi/index.js
+++ Tutorials_Node/RestApi/index.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const users = require('./MOCK_DATA.json')
-
+const fs = require("fs")
 const app = express();
 
 const PORT = 5555 ; 
+//MiddleWares -> Plugins
+app.use(express.urlencoded({extended : false}));
 
 //routes
-app.get("/api/users" , (req, res)=> {
-    return res.json(users) ;
+ app.get("/api/users" , (req, res)=> {
+     return res.json(users) ;
 })
-// app.get("/users" , (req, res)=> {
-//     const html =   `
-//     <ul>
-//         ${users.map((user)=> `<li> ${user.first_name}</li>`).join("")}
-//     </ul>
-//     `
-//     res.send(html);
-//})
 
 
 
 app
     .route("/api/users/:id")
-    .get("/api/users/:id" , (req, res)=> {
+    .get( (req, res)=> {
         const id = Number(req.params.id);
         const user = users.find(user =>  user.id ===id );
         return res.json(user);
@@ -62,7 +56,14 @@ app
 
 app.post("/api/users" , (req,res)=>{
     //TODO create a new user
-    return res.json({status : "status pending"});
+    const body = req.body;
+   console.log("Body" , body)
+ //  now adding this to my  mock data
+    users.push({...body ,  id : users.length+1});
+    fs.writeFile('./MOCK_DATA.json' , JSON.stringify(users) , (err , data)=> {
+        return res.json({status : "Success" , id:users.length});
+
+    })
     
 })
 
