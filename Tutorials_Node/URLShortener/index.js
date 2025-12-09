ed77a27d632e73f7e10012dad1661b86603c26bd
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-10 01:15:28 +0530

    Nano id used  with whoel post request

--- Tutorials_Node/URLShortener/index.js
+++ Tutorials_Node/URLShortener/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
+const {connectToMongoDB} = require("./connection")
+const urlRoute = require("./routes/url")
 const app = express();
 const PORT = 8001 ; 
 
+connectToMongoDB('mongodb://localhost:127.0.0.1:27017/short-url')
+.then(()=>{
+    console.log("mongo DB Connected")
+});
+
+app.use("/url" , urlRoute);
 app.listen(PORT , ()=> console.log(`Server Started at PORT ${PORT}`));
