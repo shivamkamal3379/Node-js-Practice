5ea6bb157728ee6de41e473f35ed758d93eb82f3
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-11 15:35:03 +0530

    Outputs displayed with ui integration

--- Tutorials_Node/URLShortener/routes/staticRouter.js
+++ Tutorials_Node/URLShortener/routes/staticRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router()
+const URL = require("../models/url")
 
 
-router.get("/", (req, res)=>{
-    return res.render("home")
+router.get("/", async(req, res)=>{
+    const allurls = await URL.find({})
+
+    return res.render("home" , {
+        urls: allurls,
+    })
 })
 
 module.exports = router;
