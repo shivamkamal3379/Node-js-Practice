dae79cbc2a69f7b8556456869de0c2ca4775757d
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-16 16:28:44 +0530

    Stateeless authentcation achived via JWT Token

--- Tutorials_Node/URLShortener/service/auth.js
+++ Tutorials_Node/URLShortener/service/auth.js
@@ -1,12 +1,23 @@
-const sessionIdToUserMap = new Map();
+ 
+const jwt = require ("jsonwebtoken")
+const secret = "Shivamkamal$1708"
 
-function setUser(id , user){
-    sessionIdToUserMap.set(id , user)
+function setUser( user){
+
+   
+    return jwt.sign({
+        _id : user._id ,
+        email: user.email,
+    }, secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
-
+function getUser(token){
+    if(!token) return null;
+    try {
+        return jwt.verify(token , secret);
+    } catch (err) {
+        return null;
+    }
     
 }
 
