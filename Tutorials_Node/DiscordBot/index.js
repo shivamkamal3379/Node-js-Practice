8b627e4f369da3f8eee4c643b7381a309df3a62d
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-18 18:11:37 +0530

    guild messages and gateway intent set up

--- Tutorials_Node/DiscordBot/index.js
+++ Tutorials_Node/DiscordBot/index.js
@@ -1,2 +1,19 @@
-
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+
+
+const client = new Client({ intents:
+     [GatewayIntentBits.Guilds ,
+     GatewayIntentBits.GuildMessages ,
+     GatewayIntentBits.MessageContent
+    ] });
+
+
+client.on('messageCreate' , (message) =>{
+    console.log(message.content);
+
+})
+
+client.login(
+"MTQ1MTEyMTQzMDg3NDk1MTc1MA.GjTkAq.ldrFYdtjezN6YeHfBVfcU4d9TKkSKmICb_k-q0"
+);
