181818025317f5aa1d41c4f5e2d3d0f50ce4bfab
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-20 23:39:53 +0530

    Initiated Blog App

---
+++ Tutorials_Node/Blog_App/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path")
+
+const app = express()
+
+const PORT = 8000;
+
+app.set("view engine" , "ejs")
+app.set("views" , path.resolve("./views"));
+
+app.listen(PORT , ()=>{
+    console.log(`Server Started at PORT : ${PORT}`)
+})
