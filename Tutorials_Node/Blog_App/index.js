e8dc12700cfde47e3d50630475aec037f6b82d95
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-21 11:05:53 +0530

    Partials added for nav with bootstrap

--- Tutorials_Node/Blog_App/index.js
+++ Tutorials_Node/Blog_App/index.js
@@ -8,6 +8,11 @@ const PORT = 8000;
 app.set("view engine" , "ejs")
 app.set("views" , path.resolve("./views"));
 
+
+app.get("/" ,(req,res)=>{
+    res.render("home");
+} )
+
 app.listen(PORT , ()=>{
     console.log(`Server Started at PORT : ${PORT}`)
 })
