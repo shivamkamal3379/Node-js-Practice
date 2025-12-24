4d4d8861ccd9ec4442a8bfcf41b3232c339f9722
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-24 17:40:09 +0530

    error soughted out

--- Tutorials_Node/Blog_App/models/user.js
+++ Tutorials_Node/Blog_App/models/user.js
@@ -40,9 +40,9 @@ const userSchema = new Schema ({
 })
 
 
-userSchema.pre('save' , function(next){
+userSchema.pre('save' ,  function(next){
     const user = this;
-    if(!user.isModified('password')) return;
+    if(!user.isModified('password')) return next() ;
 
     const salt = randomBytes(16).toString();
     const hashedPassword = createHmac('sha256' , salt  )
@@ -52,10 +52,31 @@ userSchema.pre('save' , function(next){
     this.salt= salt ; 
     this.password = hashedPassword;
 
+    next();
 
 
+});
 
-})
+
+userSchema.static("matchPassword" , async function(email , password){
+    const user = await  this.findOne({email});
+    if(!user) throw new Error("User Not Found !!") ; 
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac('sha256' , salt  )
+    .update(password)
+    .digest("hex");
+
+    if(hashedPassword !== userProvidedHash) 
+        throw new Error("Incorrect Password ");
+
+    return {...user , password: undefined , salt : undefined};
+}
+)
+
+
 
 const User = model("user" , userSchema);
 module.exports = User;
