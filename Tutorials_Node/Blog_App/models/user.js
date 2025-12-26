1228caff4021755b2ae5752bfe02b8ae3180a8a3
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-26 18:55:20 +0530

    next bug rectified

--- Tutorials_Node/Blog_App/models/user.js
+++ Tutorials_Node/Blog_App/models/user.js
@@ -1,82 +1,75 @@
-const {Schema , model}= require("mongoose");
-const {createHmac , randomBytes} = require("crypto");
+const { createHmac, randomBytes } = require("crypto");
+const { Schema, model } = require("mongoose");
+// const { createTokenForUser } = require("../services/authentication");
 
-
-
-const userSchema = new Schema ({
-    fullName :{
-        type: String ,
-        required: true ,
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-
-     email :{
-        type: String ,
-        required: true ,
-        unique:true ,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-
-    salt:{
-        type: String ,
-     },
-    password :{
-        type: String ,
-        required: true ,
-        unique:true ,
-
+    salt: {
+      type: String,
     },
-    profileImageURL:{
-        type: String , 
-        default:"/images/default.png",
-
+    password: {
+      type: String,
+      required: true,
     },
-    role:{
-        type : String,
-        enum: ["USER" , "ADMIN"],
-        default : "USER",
-    }
-},
-{
-    timestamps : true ,
-})
+    profileImageURL: {
+      type: String,
+      default: "/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
 
+userSchema.pre("save", function (next) {
+  const user = this;
 
-userSchema.pre('save' ,  function(next){
-    const user = this;
-    if(!user.isModified('password')) return next() ;
+  if (!user.isModified("password")) return;
 
-    const salt = randomBytes(16).toString();
-    const hashedPassword = createHmac('sha256' , salt  )
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
     .update(user.password)
     .digest("hex");
 
-    this.salt= salt ; 
-    this.password = hashedPassword;
+  this.salt = salt;
+  this.password = hashedPassword;
 
-    next();
-
-
+  next();
 });
 
-
-userSchema.static("matchPassword" , async function(email , password){
-    const user = await  this.findOne({email});
-    if(!user) throw new Error("User Not Found !!") ; 
+userSchema.static(
+  "matchPasswordAndGenerateToken",
+  async function (email, password) {
+    const user = await this.findOne({ email });
+    if (!user) throw new Error("User not found!");
 
     const salt = user.salt;
     const hashedPassword = user.password;
 
-    const userProvidedHash = createHmac('sha256' , salt  )
-    .update(password)
-    .digest("hex");
+    const userProvidedHash = createHmac("sha256", salt)
+      .update(password)
+      .digest("hex");
 
-    if(hashedPassword !== userProvidedHash) 
-        throw new Error("Incorrect Password ");
+    if (hashedPassword !== userProvidedHash)
+      throw new Error("Incorrect Password");
 
-    return {...user , password: undefined , salt : undefined};
-}
-)
+    // const token = createTokenForUser(user);
+    return token;
+  }
+);
 
+const User = model("user", userSchema);
 
-
-const User = model("user" , userSchema);
 module.exports = User;
